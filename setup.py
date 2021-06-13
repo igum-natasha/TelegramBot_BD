c288bf4379327cd22373088c4d3c3a0576c08194
natasha <igum.natasha@gmail.com>
2021-06-13 10:23:53 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,2 @@
+TOKEN = '1776959723:AAHeY_-ueFBFsgzHW7uRQcVzoRzuZREK3lg'  # insert your token here
+PROXY = 'tg://proxy?server=proxy.digitalresistance.dog&port=443&secret=ddd41d8cd98f00b204e9800998ecf8427e'
